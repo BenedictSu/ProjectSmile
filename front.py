8c46aecc1a3c1dd821ede452e9aae071525b370b
BenedictSu <BenedictTheGreat@hotmail.com>
2014-07-26 14:25:27 +0800

    Bootstrap
    -Applied bootstrap using an online template
    -Attempted to implement the logging in but not completed yet

--- front.py
+++ front.py
@@ -1,18 +1,35 @@
+import urllib
 import webapp2
 import jinja2
 import os
+import datetime
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-
-class MainPage(webapp2.RequestHandler):
+# This part for the Home Page
+class HomePage(webapp2.RequestHandler):
     """ Handler for the front page."""
 
     def get(self):
         template = jinja_environment.get_template('Home.html')
         self.response.out.write(template.render())
 
+class MainPage(webapp2.RequestHandler):
+    # Main page for those logged in
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+    def get(self):
+        user = users.get_current_user()
+        if user:  # signed in already
+            template_values = {
+                'user_mail': users.get_current_user().email(),
+                'logout': users.create_logout_url(self.request.host_url),
+            }
+            template = jinja_environment.get_template('Main.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            self.redirect(self.request.host_url)
+			
+app = webapp2.WSGIApplication([('/', HomePage),
+							   ('/main', MainPage)],
                               debug=True)
